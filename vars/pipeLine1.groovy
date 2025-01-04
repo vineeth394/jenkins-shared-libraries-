dd4d1a47bedf33a6ec4bd7521936a2e8091477f5
vineeth394 <munnavineeth@gmail.com>
2025-01-04 12:30:30 +0530

    Update2 pipeLine1.groovy

--- vars/pipeLine1.groovy
+++ vars/pipeLine1.groovy
@@ -3,18 +3,18 @@ def call() {
     checkout scm
 }
 
-def call() {
+ {
     echo 'Setting up Java 17...'
     sh 'sudo apt update'
     sh 'sudo apt install -y openjdk-17-jdk'
 }
 
-def call() {
+ {
     echo 'Setting up Maven...'
     sh 'sudo apt install -y maven'
 }
 
-def call() {
+ {
     echo 'Building project with Maven...'
     sh 'mvn clean package'
 }
