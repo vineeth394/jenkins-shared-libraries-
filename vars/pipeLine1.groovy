696c8f9528b8683d82a8cbc19c8dc9b57576a2a1
vineeth394 <munnavineeth@gmail.com>
2025-01-04 12:53:51 +0530

    runapp pipeLine1.groovy

--- vars/pipeLine1.groovy
+++ vars/pipeLine1.groovy
@@ -14,4 +14,11 @@ def call() {
     
     echo 'Uploading artifact...'
     archiveArtifacts artifacts: 'target/*.jar', allowEmptyArchive: true
+   
+    echo 'Running Spring Boot application...'
+    sh 'nohup mvn spring-boot:run &'
+    sleep(time: 15, unit: 'SECONDS')
+
+    def publicIp = sh(script: "curl -s https://checkip.amazonaws.com", returnStdout: true).trim()
+    echo "The application is running and accessible at: http://${publicIp}:8080"
 }
